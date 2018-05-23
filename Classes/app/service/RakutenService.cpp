e9026ec2627c02e96d0eb5b872e22dbfebf6ae65
noviceky <rational.instinct@gmail.com>
2018-05-23 13:49:54 +0900

    RakutenServiceの追加

---
+++ Classes/app/service/RakutenService.cpp
@@ -0,0 +1,28 @@
+//
+//  RakutenService.cpp
+//  rakuran-mobile
+//
+//  Created by kyagi on 2018/05/23.
+//
+
+#include "RakutenService.hpp"
+#include "AppMacro.h"
+
+RakutenService* RakutenService::_instance = NULL;
+
+RakutenService::RakutenService()
+{
+}
+
+RakutenService* RakutenService::getInstance()
+{
+    if (NULL == _instance)
+    {
+        _instance = new RakutenService();
+    }
+    return _instance;
+}
+
+void RakutenService::destroy()
+{
+}
