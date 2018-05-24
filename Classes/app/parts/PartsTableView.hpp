55cf2753f3d5e530900dfd75011e26dd4f6d92da
noviceky <rational.instinct@gmail.com>
2018-05-24 13:51:19 +0900

    ファイル追加/ほぼコピペ

---
+++ Classes/app/parts/PartsTableView.hpp
@@ -0,0 +1,63 @@
+//
+//  PartsTableView.hpp
+//  rakuran-mobile
+//
+//  Created by kyagi on 2018/05/24.
+//
+
+#ifndef __rakuran__PartsTableView__
+#define __rakuran__PartsTableView__
+
+#include "cocos2d.h"
+#include "editor-support/cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+#include <extensions/cocos-ext.h>
+#include "RankInfoDTO.h"
+
+USING_NS_CC;
+using namespace ui;
+using namespace extension;
+
+struct RankInfoDTO;
+
+class PartsTableView
+: public Layout
+, public TableViewDataSource
+, public TableViewDelegate
+{
+public:
+    static PartsTableView* create(const Size& size);
+    
+    void setRankInfoDTOList(const std::vector<RankInfoDTO>& list);
+    
+    using OnSelected = std::function<void(const int idx)>;
+    CC_SYNTHESIZE(OnSelected, _onSelected, OnSelected);
+    
+private:
+    PartsTableView();
+    PartsTableView(const Size& size);
+    virtual ~PartsTableView();
+    virtual bool init() override;
+    virtual void onEnter() override;
+    virtual void onEnterTransitionDidFinish() override;
+    virtual void onExit() override;
+    virtual void onExitTransitionDidStart() override;
+    
+    void initUI();
+    
+private:
+    // TableViewDataSourceの抽象メソッド
+    virtual Size           tableCellSizeForIndex(TableView* table, ssize_t idx) override;
+    virtual TableViewCell* tableCellAtIndex(TableView* table, ssize_t idx) override;
+    virtual ssize_t        numberOfCellsInTableView(TableView* table) override;
+    
+    // TableViewDelegateの抽象メソッド
+    virtual void tableCellTouched(TableView* table, TableViewCell* cell) override;
+    
+private:
+    TableView*             _tableView;
+    Size                   _tableViewContentSize;
+    std::vector<RankInfoDTO> _rankInfoDTOList;
+};
+
+#endif /* defined(__rakuran__PartsTableView__) */
---
+++ Classes/app/parts/PartsTableViewCell.hpp
@@ -0,0 +1,35 @@
+//
+//  PartsTableViewCell.hpp
+//  rakuran-mobile
+//
+//  Created by kyagi on 2018/05/24.
+//
+
+#ifndef __rakuran__PartsTableViewCell__
+#define __rakuran__PartsTableViewCell__
+
+#include "cocos2d.h"
+#include "editor-support/cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+using namespace ui;
+
+class PartsTableViewCell : public Layout
+{
+public:
+    CREATE_FUNC(PartsTableViewCell);
+    
+    void initWithViewCellInfo(const int idx, const Size& size, const std::string& title);
+    
+private:
+    PartsTableViewCell();
+    virtual ~PartsTableViewCell();
+    virtual bool init() override;
+    virtual void onEnter() override;
+    virtual void onEnterTransitionDidFinish() override;
+    virtual void onExit() override;
+    virtual void onExitTransitionDidStart() override;
+};
+
+#endif /* defined(__rakuran__PartsTableViewCell__) */
